c908c48be88975799379abd2452a55d890825ac4
bbland1 <104288486+bbland1@users.noreply.github.com>
2023-05-27 14:20:43 -0400

    inital github actions setup and linting

--- src/__tests__/Links.test.js
+++ src/__tests__/Links.test.js
@@ -1,35 +1,35 @@
-import { render, screen } from "@testing-library/react"
-import Links from "../Components/Links/LinksPage"
-import { BrowserRouter } from "react-router-dom";
-import '@testing-library/jest-dom'
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Links from '../Components/Links/LinksPage';
+import '@testing-library/jest-dom';
 
 // testing for the links at the bottom of the page
-test("Anilist Link", () => {
-  render(<Links/>, {wrapper: BrowserRouter}
-  );
+test('Anilist Link', () => {
+  render(<Links />, { wrapper: BrowserRouter });
 
-  const anilistLink = screen.getByTestId("Anilist-Link");
+  const anilistLink = screen.getByTestId('Anilist-Link');
   expect(anilistLink).toBeInTheDocument();
 });
 
-test("Anilist Correct Url", () => {
-  render(<Links/>, {wrapper: BrowserRouter});
+test('Anilist Correct Url', () => {
+  render(<Links />, { wrapper: BrowserRouter });
 
-  const anilistLink = screen.getByTestId("Anilist-Link");
-  expect(anilistLink.href).toContain("https://anilist.co/");
+  const anilistLink = screen.getByTestId('Anilist-Link');
+  expect(anilistLink.href).toContain('https://anilist.co/');
 });
 
-test("My Anime List Link", () => {
-  render(<Links/>, {wrapper: BrowserRouter}
-  );
+test('My Anime List Link', () => {
+  render(<Links />, { wrapper: BrowserRouter });
 
-  const myAnimeListLink = screen.getByTestId("MAL-Link");
+  const myAnimeListLink = screen.getByTestId('MAL-Link');
   expect(myAnimeListLink).toBeInTheDocument();
 });
 
-test("My Anime List Correct Url", () => {
-  render(<Links/>, {wrapper: BrowserRouter});
+test('My Anime List Correct Url', () => {
+  render(<Links />, { wrapper: BrowserRouter });
 
-  const  myAnimeListLink = screen.getByTestId("MAL-Link");
-  expect( myAnimeListLink.href).toContain("https://myanimelist.net/");
-})
+  const myAnimeListLink = screen.getByTestId('MAL-Link');
+  expect(myAnimeListLink.href).toContain('https://myanimelist.net/');
+});
